f67efda82b7fff0c4b74612c9ef3acad78f25f13
umarmukhthar21 <98202837+umarmukhthar21@users.noreply.github.com>
2022-05-06 10:05:38 +0530

    Add files via upload

---
+++ src/data/user.js
@@ -0,0 +1,19 @@
+var users=[{
+    "id":1,
+    "uid":"aiswaryawalter@gmail.com",
+    "pwd":"aiswarya"
+},
+{
+    "id":2,
+    "uid":"surya@gmail.com",
+    "pwd":"surya"
+},
+{
+    "id":3,
+    "uid":"bazith@gmail.com",
+    "pwd":"bazith"
+}
+]
+
+
+module.exports=users;// user changed to users
