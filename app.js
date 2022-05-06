cb5c729ac5320ebe59ea74346a27e955485dfd4e
umarmukhthar21 <98202837+umarmukhthar21@users.noreply.github.com>
2022-05-06 10:03:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express'); 
+const path = require ('path'); 
+const cors = require('cors');
+
+const nav= [
+    {
+        link:"/books",
+        title:"Books"
+    },
+    {
+        link:"/authors",
+        title:"Authors"
+    },
+    {
+        link:"/addbook",
+        title:"Add Book"
+    },
+    {
+        link:"/addauthor",
+        title:"Add Author"
+    }
+]
+
+const loginRouter = require('./src/routes/loginroute');
+const signupRouter = require('./src/routes/signuproute');
+const homeRouter = require('./src/routes/homerouter');// homerouter speling not correct
+const booksRouter = require('./src/routes/booksroute');
+const authorsRouter = require('./src/routes/authorsroute');
+
+const app = new express; 
+
+
+app.set('views','./src/views'); 
+app.set('view engine','ejs'); 
+
+
+app.use(express.urlencoded({extended:true}));// bodypaser to changed to express
+app.use(express.json());
+app.use(express.static(path.join(__dirname , '/public'))); 
+
+app.use('/login',loginRouter); 
+app.use('/signup',signupRouter); 
+app.use('/home',homeRouter); 
+app.use('/books',booksRouter); 
+app.use('/authors',authorsRouter); 
+
+
+
+app.get('/',function(req,res){
+
+    res.render('index',{});
+    
+});
+
+
+
+
+
+app.listen(5000,()=>{
+    console.log("Server Ready on 3000");
+});
